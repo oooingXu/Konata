6054014b877bb7ddb963833d857bd3493619e0c3
oooing <202301202097@stumail.sztu.edu.cn>
2026-02-05 11:40:46 +0800

    chmod install.sh and konata.sh +x

--- konata.sh
+++ konata.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -e $(dirname $0)/node_modules ]; then
-    npx --prefix $(dirname $0) electron $(dirname $0) $1
+    npx --prefix $(dirname $0) electron $(dirname $0) $1 --no-sandbox
 else
     echo "'node_modules' does not exit. Please install node.js and type 'node install' in this directory."-
 fi 
